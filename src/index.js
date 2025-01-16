a576899fb95703f026226da92a2c2c14219da73b
Austin <austinlandry271@gmail.com>
2025-01-16 19:13:35 +1100

    Initialised the project with some of the packages and server files.

---
+++ src/index.js
@@ -0,0 +1,4 @@
+require('dotenv').config({ path: __dirname + '/../.env' });
+// require('dotenv').config();
+var PORT = process.env.PORT;
+console.log("THE PORT IS = ".concat(PORT));
---
+++ src/index.ts
@@ -0,0 +1,7 @@
+require('dotenv').config({path: __dirname + '/../.env'});
+// require('dotenv').config();
+
+const PORT = process.env.PORT
+
+
+console.log(`THE PORT IS = ${PORT}`)
